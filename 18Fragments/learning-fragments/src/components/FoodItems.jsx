a0b64b71c9f79e155e017c2bf1969dbc15ba3a2d
Tejas Bhosale <50192323+tejasbhosale17@users.noreply.github.com>
2024-02-08 11:55:20 +0530

    Completed  State Management {useState Hook to manage Component State, State vs Props}

--- 18Fragments/learning-fragments/src/components/FoodItems.jsx
+++ 18Fragments/learning-fragments/src/components/FoodItems.jsx
@@ -1,12 +1,21 @@
 import Item from "./Item";
+import { useState } from "react";
 const FoodItems = ({ FoodItems }) => {
+  let [activeItems, setActiveItems] = useState([]);
+
+  let onBuyButton = (item, event) => {
+    let newItems = [...activeItems, item];
+    setActiveItems(newItems);
+  };
+
   return (
     <ul className="list-group">
       {FoodItems.map((item) => (
         <Item
           key={item}
           foodItem={item}
-          handleBuyButton={() => console.log(`${item} bought`)}
+          bought={activeItems.includes(item)}
+          handleBuyButton={() => onBuyButton(item, event)}
         ></Item>
       ))}
     </ul>
--- 18Fragments/learning-fragments/src/components/Item.jsx
+++ 18Fragments/learning-fragments/src/components/Item.jsx
@@ -1,6 +1,6 @@
 // const Item = (props) => {
 import styles from "./Item.module.css";
-const Item = ({ foodItem, handleBuyButton }) => {
+const Item = ({ foodItem, bought, handleBuyButton }) => {
   // console.log(styles);
   // let { fooditem } = props;
   // const buyButton = (event) => {
@@ -10,7 +10,9 @@ const Item = ({ foodItem, handleBuyButton }) => {
 
   return (
     <>
-      <li className={`${styles["kg-item"]} list-group-item`}>
+      <li
+        className={`${styles["kg-item"]} list-group-item ${bought && "active"}`}
+      >
         <span className={styles["kg-span"]}>{foodItem}</span>
         <button
           className={`${styles.button} btn btn-info`}
