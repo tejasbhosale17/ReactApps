583debd7cb6285d35f15ebcb2817edf28ad4a648
Tejas Bhosale <50192323+tejasbhosale17@users.noreply.github.com>
2024-08-08 19:22:50 +0530

    Completed Intro and skills section in About

--- Projects/portfolio-website/src/components/About.jsx
+++ Projects/portfolio-website/src/components/About.jsx
@@ -1,9 +1,10 @@
 import profileImg from "../assets/2faceImg/newImages/firstImg.png";
-
+import "./styles/about.min.css";
+import skillPiChartImg from "../assets/skill-piChart.png";
 const About = () => {
   return (
     <div className="about">
-      <section>
+      <section className="intro" style={{ backgroundColor: "white" }}>
         <div className="container col-xxl-8 px-4 py-5">
           <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
             <div className="col-10 col-sm-8 col-lg-6">
@@ -16,16 +17,15 @@ const About = () => {
                 loading="lazy"
               />
             </div>
-            <div className="col-lg-6">
-              <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">
-                Responsive left-aligned hero with image
-              </h1>
-              <p className="lead">
-                Quickly design and customize responsive mobile-first sites with
-                Bootstrap, the world’s most popular front-end open source
-                toolkit, featuring Sass variables and mixins, responsive grid
-                system, extensive prebuilt components, and powerful JavaScript
-                plugins.
+            <div className="col-lg-6 desc-intro">
+              <h1 className="display-5 lh-1 mb-3">about.</h1>
+              <p className="intro-heading">
+                I'm a Computer Engineer based in pune Maharashtra, India.{" "}
+              </p>
+              <p className="description">
+                Since 2017, I've enjoyed working on various projects. When I'm
+                not in front of a screen, you'll find me cooking, traveling or
+                working out in the gym.
               </p>
               <div className="d-grid gap-2 d-md-flex justify-content-md-start">
                 <button
@@ -40,6 +40,37 @@ const About = () => {
                 >
                   Default
                 </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="skill-chart">
+        <div className="tech-skills">
+          <div className="row">
+            <div className="col-12 pie-chart">
+              <div className="developer">
+                <h2>Developer</h2>
+                <ul>
+                  <li>ReactJs</li>
+                  <li>SpringBoot</li>
+                  <li>AWS</li>
+                  <li>Linux</li>
+                  <li>Git/GitHub</li>
+                </ul>
+              </div>
+              <div className="chart">
+                <img src={skillPiChartImg} style={{ width: "400px" }} />
+              </div>
+              <div className="coder">
+                <h2>Coder</h2>
+                <ul>
+                  <li>Java</li>
+                  <li>C++</li>
+                  <li>MySQL</li>
+                  <li>HTML/CSS</li>
+                  <li>JavaScript</li>
+                </ul>
               </div>
             </div>
           </div>
